fc92d795ad690e363718a29174eb615c14fe7772
Christopher Lepore <christopher.r.lepore@gmail.com>
2012-10-12 21:22:53 -0400

    Styled it something pink and gray. Also removed bootstrap as we didn't need it.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 (function() {
+  // Prototype to style tweet parts
+  String.prototype.style_tweet = function() {
+    var tweet = this.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/g);
+    tweet = tweet.replace(/(^|\s)@(\w+)/g, '$1<span class="username">@$2</span>');
+    return tweet.replace(/(^|\s)#(\w+)/g, '$1<span class="hashtag">#$2</span>');
+ };
 
   var lastTweetLoaded = 0;
   
@@ -7,29 +13,26 @@
       url: 'tweets.txt', 
       dataType: 'text',
       success: function(data) {
-        
-        var str = data;
-        var tweets = $.parseJSON('[' + str + ']');
-        var $list = $('#tweets');
-        var startPrepending = false;
-        $(tweets).each(function(idx, tweet) {
+        var str = data,
+            tweets = $.parseJSON('[' + str + ']'),
+            $list = $('#tweets'),
+            startPrepending = false;
 
+        $(tweets).each(function(idx, tweet) {
           if (lastTweetLoaded == 0 || tweet.id > lastTweetLoaded) {
-            var li = '<li>' + tweet.text + '</li>';
-            $list.prepend(li);
+            var str = tweet.text;
+            $list.prepend('<li>' + str.style_tweet(str) + '</li>');
             startPrepending = true;
           }
           lastTweetLoaded = tweet.id;
         });
-       },
-      error: function(data) {
-        console.log(data);
-      }
+      },
+      error: function(data) { console.log('An error has occured: ' + data); }
     });
   }
   
   $(document).ready(function() {
-      setInterval(loadTweets, 2000);
+    loadTweets();
+    //setInterval(loadTweets, 2000);
   });
-
 })();
